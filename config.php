37749f3613fda28e4efce2bc9b51321cc89f314a
Antonio Scarpino <34765237+antonioscarpino@users.noreply.github.com>
2018-03-23 21:57:31 +0100

    upload file config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+    //dati pre la connessione al database
+    $user = 'il_tuo_user_per_la_connessione_al_DB';
+    $pass = 'la_tua_pawwsord_per_la_connessione_al_DB';
+    $host = 'host_del_DB';
+    $db = 'nome_DB';
