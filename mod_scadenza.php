1d8c7792cbd3a94f378cf58a440bdf7308068bd2
antonioscarpino <34765237+antonioscarpino@users.noreply.github.com>
2018-03-23 23:44:07 +0100

    eliminato dalla nav-bar il menù scadenze

--- mod_scadenza.php
+++ mod_scadenza.php
@@ -50,12 +50,6 @@ if ($_GET) {
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav navbar-right">
             <li><a href="index.php">Home </a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Scadenze <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="agg_scadenza.php">Aggiungi Scadenza </a></li>
-              </ul>
-            </li>
           </ul>
         </div>
         <!-- /.navbar-collapse -->
